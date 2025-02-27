e38b138d2975e9c9a3668f23fd95c9de21e29920
Chris Schertenlieb <schertenlieb@urbaninsight.com>
2025-02-27 17:31:36 -0500

    figured out tab endpoints, broke into two sections, updated field types to match requirements

--- ncma-annotated-image-register-posttype.php
+++ ncma-annotated-image-register-posttype.php
@@ -86,211 +86,157 @@ if( function_exists('acf_add_local_field_group') ):
             ),
         ),
     );
-
-    /* Group for fields that do not change with language */
     acf_add_local_field_group(array(
-        'key' => 'annotated-image-universal',
-        'title' => 'Annotated Image - Text - English only',
-        'fields' => array (
-            /* English tab */
+        'key' => 'annotated-image-text-info',
+        'title' => 'Annotated Image - Image Info',
+        'menu_order' => 0,
+        'fields' => array(
+            /* Title */
             array(
-                'key' => 'field_ncma_annotated_image_en_text_tab',
+                'key' => 'field_ncma_annotated_image_title',
+                'label' => 'Title',
+                'name' => 'ncma_annotated_image_title',
+                'type' => 'text',
+                'instructions' => '',
+                'required' => 1,
+                'conditional_logic' => 0,
+            ),
+
+            /* Tab Group Start for Top-Level Description */
+            array(
+                'key' => 'field_ncma_annotated_image_en_tab',
                 'label' => 'English',
                 'name' => '',
                 'type' => 'tab',
-                'instructions' => '',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'wrapper' => array(
-                    'width' => '',
-                    'class' => '',
-                    'id' => '',
-                ),
                 'placement' => 'top',
-                'endpoint' => 0,
             ),
-            /* English fields */
-            array (
-                'key' => 'field_ncma_annotated_image_en_heading',
-                'label' => 'Heading',
-                'name' => 'ncma_annotated_image_en_heading',
-                'type' => 'text',
-                'instructions' => 'This heading appears above the prompts on the prompt selection menu.',
+            array(
+                'key' => 'field_ncma_annotated_image_en_description',
+                'label' => 'Description (English)',
+                'name' => 'ncma_annotated_image_en_description',
+                'type' => 'textarea',
                 'required' => 1,
-                'conditional_logic' => 0,
             ),
-            array (
-                'key' => 'field_ncma_annotated_image_en_prompt_1',
-                'label' => 'Prompt 1',
-                'name' => 'ncma_annotated_image_en_prompt_1',
-                'type' => 'text',
-                'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
+            array(
+                'key' => 'field_ncma_annotated_image_es_tab',
+                'label' => 'Spanish',
+                'name' => '',
+                'type' => 'tab',
+                'placement' => 'top',
+            ),
+            array(
+                'key' => 'field_ncma_annotated_image_es_description',
+                'label' => 'Description (Spanish)',
+                'name' => 'ncma_annotated_image_es_description',
+                'type' => 'textarea',
                 'required' => 1,
-                'conditional_logic' => 0,
             ),
-            array (
-                'key' => 'field_ncma_annotated_image_en_prompt_2',
-                'label' => 'Prompt 2',
-                'name' => 'ncma_annotated_image_en_prompt_2',
-                'type' => 'text',
-                'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
-                'required' => 0,
-                'conditional_logic' => 0,
-            ),
-            array (
-                'key' => 'field_ncma_annotated_image_en_prompt_3',
-                'label' => 'Prompt 3',
-                'name' => 'ncma_annotated_image_en_prompt_3',
-                'type' => 'text',
-                'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
-                'required' => 0,
-                'conditional_logic' => 0,
-            ),
-            /* Spanish tab */
-            // array(
-            //     'key' => 'field_ncma_annotated_image_es_text_tab',
-            //     'label' => 'Spanish',
-            //     'name' => '',
-            //     'type' => 'tab',
-            //     'instructions' => '',
-            //     'required' => 0,
-            //     'conditional_logic' => 0,
-            //     'wrapper' => array(
-            //         'width' => '',
-            //         'class' => '',
-            //         'id' => '',
-            //     ),
-            //     'placement' => 'top',
-            //     'endpoint' => 0,
-            // ),
-            /*Spanish fields*/
-            // array (
-            //     'key' => 'field_ncma_annotated_image_es_prompt_1',
-            //     'label' => 'Prompt 1',
-            //     'name' => 'ncma_annotated_image_es_prompt_1',
-            //     'type' => 'text',
-            //     'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
-            //     'required' => 0,
-            //     'conditional_logic' => 0,
-            // ),
-            // array (
-            //     'key' => 'field_ncma_annotated_image_es_prompt_2',
-            //     'label' => 'Prompt 2',
-            //     'name' => 'ncma_annotated_image_es_prompt_2',
-            //     'type' => 'text',
-            //     'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
-            //     'required' => 0,
-            //     'conditional_logic' => 0,
-            // ),
-            // array (
-            //     'key' => 'field_ncma_annotated_image_es_prompt_3',
-            //     'label' => 'Prompt 3',
-            //     'name' => 'ncma_annotated_image_es_prompt_3',
-            //     'type' => 'text',
-            //     'instructions' => 'An evocative prompt related to exhibit themes for visitors to respond to.',
-            //     'required' => 0,
-            //     'conditional_logic' => 0,
-            // ),
+            array(
+                'key' => 'field_ncma_annotated_image_info_text_tab_endpoint',
+                'type' => 'tab',
+                'endpoint' => true,
+            )
+            /* Tab Group End */
+
+            /* Additional fields can be placed here after the tab groups */
         ),
-        'location' => $location,
+        'location' => $location
     ));
 
-     /* Field group for QR code and help text content - English, Spanish */
-     acf_add_local_field_group(array(
-        'key' => 'annotated-image-qr-code',
-        'title' => 'Annotated Image - QR Code - English only',
-        'fields' => array (
-            /* QR Code tab */
+    acf_add_local_field_group(array(
+        'key' => 'annotated-image-annotations',
+        'title' => 'Annotated Image - Image & Annotations',
+        'menu_order' => 1,
+        'fields' => array(
+            /* Image */
             array(
-                'key' => 'field_ncma_annotated_image_qr_code_tab',
-                'label' => 'QR Code',
-                'name' => '',
-                'type' => 'tab',
-                'instructions' => '',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'wrapper' => array(
-                    'width' => '',
-                    'class' => '',
-                    'id' => '',
-                ),
-                'placement' => 'top',
-                'endpoint' => 0,
-            ),
-            /* QR Code image field */
-            array (
-                'key' => 'field_ncma_annotated_image_qr_code',
-                'label' => 'QR Code',
-                'name' => 'ncma_annotated_image_qr_code',
-                'type' => 'relationship',
-                'instructions' => "A QR code for visitors to submit responses from their personal device. The QR code is published below the prompt selection menu. <br />This Annotated Image will be published at <span style='color:indianred;'>https://ncma-kiosks.pages.dev/response/{$post_id}</span>. Make sure your QR code links to this address.",
-                'required' => 0,
-                'min' => 0,
-                'max' => 1,
-                'post_type' => 'ncma-qr-code',
-                'filters' => array('search'),
-                'elements' => array('featured_image'),
+                'key' => 'field_ncma_annotated_image',
+                'label' => 'Image',
+                'name' => 'ncma_annotated_image',
+                'type' => 'image',
                 'return_format' => 'id',
-                'conditional_logic' => 0,
+                'preview_size' => 'medium',
+                'library' => 'all',
             ),
-            /* English tab */
+            /* Image Annotations Repeater */
             array(
-                'key' => 'field_ncma_annotated_image_en_qr_code_tab',
-                'label' => 'English',
-                'name' => '',
-                'type' => 'tab',
-                'instructions' => '',
-                'required' => 0,
-                'conditional_logic' => 0,
-                'wrapper' => array(
-                    'width' => '',
-                    'class' => '',
-                    'id' => '',
+                'key' => 'field_ncma_annotated_image_annotations',
+                'label' => 'Image Annotations',
+                'name' => 'ncma_annotated_image_annotations',
+                'type' => 'repeater',
+                'layout' => 'block',
+                'button_label' => 'Add Annotation',
+                'sub_fields' => array(
+                    array(
+                        'key' => 'field_ncma_annotation_coordinates',
+                        'label' => 'Coordinates',
+                        'name' => 'ncma_annotation_coordinates',
+                        'type' => 'text',
+                    ),
+                    /* Tab Group Start Inside Repeater */
+                    array(
+                        'key' => 'field_ncma_annotation_en_tab',
+                        'label' => 'English',
+                        'name' => '',
+                        'type' => 'tab',
+                        'placement' => 'top',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_en_title',
+                        'label' => 'Title (English)',
+                        'name' => 'ncma_annotation_en_title',
+                        'type' => 'text',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_en_description',
+                        'label' => 'Description (English)',
+                        'name' => 'ncma_annotation_en_description',
+                        'type' => 'wysiwyg',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_es_tab',
+                        'label' => 'Spanish',
+                        'name' => '',
+                        'type' => 'tab',
+                        'placement' => 'top',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_es_title',
+                        'label' => 'Title (Spanish)',
+                        'name' => 'ncma_annotation_es_title',
+                        'type' => 'wysiwyg',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_es_description',
+                        'label' => 'Description (Spanish)',
+                        'name' => 'ncma_annotation_es_description',
+                        'type' => 'textarea',
+                        'endpoint' => true,
+                    ),
+                    array('key' => 'field_ncma_annotation_text_tab_endpoint', 'type' => 'tab', 'endpoint' => true),
+
+                    /* Tab Group End Inside Repeater */
+                    array(
+                        'key' => 'field_ncma_annotation_related_image',
+                        'label' => 'Related Image',
+                        'name' => 'ncma_annotation_related_image',
+                        'type' => 'image',
+                        'return_format' => 'id',
+                        'preview_size' => 'medium',
+                        'library' => 'all',
+                    ),
+                    array(
+                        'key' => 'field_ncma_annotation_related_caption',
+                        'label' => 'Related Image Caption',
+                        'name' => 'ncma_annotation_related_caption',
+                        'type' => 'text',
+                    ),
                 ),
-                'placement' => 'top',
-                'endpoint' => 0,
             ),
-            /* English fields */
-            array (
-                'key' => 'field_ncma_annotated_image_en_qr_code_text',
-                'label' => 'QR Code Instruction (English)',
-                'name' => 'ncma_annotated_image_en_qr_code_text',
-                'type' => 'text',
-                'instructions' => 'Text placed next to the QR code instructing users how to use it.',
-                'default_value' => 'Interested in sharing from your own device? <br />Scan the QR code to submit and answer from your phone.',
-                'required' => 0,
-                'conditional_logic' => 0,
-            ),
-            // /* Spanish tab */
-            // array(
-            //     'key' => 'field_ncma_annotated_image_es_qr_code_tab',
-            //     'label' => 'Spanish',
-            //     'name' => '',
-            //     'type' => 'tab',
-            //     'instructions' => '',
-            //     'required' => 0,
-            //     'conditional_logic' => 0,
-            //     'wrapper' => array(
-            //         'width' => '',
-            //         'class' => '',
-            //         'id' => '',
-            //     ),
-            //     'placement' => 'top',
-            //     'endpoint' => 0,
-            // ),
-            // /* Spanish fields */
-            // array (
-            //     'key' => 'field_ncma_annotated_image_es_qr_code_text',
-            //     'label' => 'Help text (Español)',
-            //     'name' => 'ncma_annotated_image_en_qr_code_text',
-            //     'type' => 'textarea',
-            //     'instructions' => 'If QR code is present, refer to the QR code. Otherwise, use generic help text.',
-            //     'required' => 1,
-            //     'conditional_logic' => 0,
-            // ),
         ),
-        'location' => $location,
+        'location' => $location
     ));
+
 
 endif;
 
